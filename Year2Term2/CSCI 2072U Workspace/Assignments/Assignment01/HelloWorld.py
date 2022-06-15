5163698ff1e2817e17bde63cdb28fe5b3004e759
Thomas Chiarello <TomChi70@outlook.com>
2022-06-14 22:35:02 -0400

    Computational Science I

---
+++ Year2Term2/CSCI 2072U Workspace/Assignments/Assignment01/HelloWorld.py
@@ -0,0 +1,9 @@
+# Assignment 1 Template file
+
+# Student Name: Thomas Chiarello
+# Student ID: 100786096
+# Collaborators (if any):
+
+def HelloWorld(name):
+  return "Hello " + name + "!"
+print(HelloWorld("u"))
